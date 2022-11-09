081dcded0dca38b809d94ff42c714a51d3233d96
p-kami-sama <paul.gonzalez@estudiantat.upc.edu>
2022-11-09 19:19:38 +0100

    Re organización de main.py

--- main.py
+++ main.py
@@ -9,40 +9,6 @@ from celula.States import States as s
 
 from automata.initial_state import initial_state
 
-'''
-    ['0', '0', '0', '4', '0', '0', '4', '4', '1', '1', '5', '2']
-    con frontera von_neumann y radio 2
-    0 -> 5 (izq o arriba)
-    1 -> 2 (der)
-    4 -> 3 (abajo)
-    5 -> (esquina abajo derecha)
-'''
-
-
-
-inicial_1 =[
-        [{'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}],
-        [{'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}],
-        [{'state': 'M'}, {'state': 'V'}, {'state': 'V'}, {'state': 'V'}, {'state': 'M'}],
-        [{'state': 'M'}, {'state': 'V'}, {'state': 'M'}, {'state': 'V'}, {'state': 'ZZ', 'variables':['sol', 'luna']}],
-        [{'state': 'M'}, {'state': 'V'}, {'state': 'V'}, {'state': 'V'}, {'state': 'YY'}],
-        [{'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}],
-        [{'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}, {'state': 'M'}]
-    ]
-
-
-def func_t(c: cell):
-    if c.state == 'V':
-        return 'M'
-    elif c.state == 'M':
-        return 'v'
-    else:
-        return 'AAAAA'
-
-
-
-
-
 
 
 
@@ -60,8 +26,7 @@ if __name__ == '__main__':
     
     
 
-    a = automata.Automata(width=10, height=5, store_trace_back=True)    #   , neighborhood='von_Neumann', border='adiabatic'
-
+    a = automata.Automata(width=10, height=5, store_trace_back=True)
 
     a.set_border(Borders.FIXED, s.Ignifugo)
 
@@ -72,6 +37,7 @@ if __name__ == '__main__':
 
     id = a.add_statistic( statistic_1_function, 'Esta ardiendo', [])
 
+# Imprime las iteraciones
     print(a.actual_iteration)
     for row in a.get_matrix_state():
         print( row )
