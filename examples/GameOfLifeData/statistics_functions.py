a08051de7ab0dfe2d17807576f16ef5abcf34266
p-kami-sama <paul.gonzalez@estudiantat.upc.edu>
2023-01-04 20:29:43 +0100

    Limpieza del código

--- examples/GameOfLifeData/statistics_functions.py
+++ examples/GameOfLifeData/statistics_functions.py
@@ -1,16 +1,17 @@
 import Cell as cell
 
-from States import States
 
+
+def statistic_function_count_30_iterations(c:cell):
+    return ( ((c.automata.actual_iteration % 30) == 0) and (c.get_variable('resurrecciones') != 0))
 
 
 statistics_message = {
     'statistic_function_count_30_iterations': 'Numero de resurrecciones',
 }
 
+
 statistics_variables = {
     'statistic_function_count_30_iterations': ['resurrecciones'],
 }
 
-def statistic_function_count_30_iterations(c:cell):
-    return ( ((c.automata.actual_iteration % 30) == 0) and (c.get_variable('resurrecciones') != 0))
